55ce35548bb90afb3c3d1ff550d1b4859df23b96
blackjerseygiant <0uk.monique@gmail.com>
2024-09-18 15:54:34 -0700

    added khmer page

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-// JavaScript for navigation between sections
+// js for navigation between sections
 function navigateTo(section) {
     const contentDiv = document.getElementById('content');
     const contactFormDiv = document.getElementById('contact-form');
     
-    // Remove the 'active' class from all nav links
+    // remove the 'active' class from all nav links
     document.querySelectorAll('nav a').forEach(link => link.classList.remove('active'));
     
-    // Clear contentDiv
+    // clear contentDiv
     contentDiv.innerHTML = '';
   
-    // Hide contact form by default
+    // hide contact form by default
     contactFormDiv.style.display = 'none';
     
-    // Switch content based on the section
+    // switch content based on the section
     if (section === 'about') {
       contentDiv.innerHTML = `
         <img src="chicken.jpg" alt="chicken digging in golden hour" class="author-image">
         <div class="bio">
           <p>Monique Ouk is a Cambodian-American writer. Her poetry has been recognized by Stanford University, nominated for the Pushcart Prize, and appears or is forthcoming in The Seventh Wave and The Margins. She has received support from Jackson Hole Writers and 4Culture. Currently, Monique is working on a novel set in the Cambodian countryside.</p>
+       <br><br>
         </div>`;
       document.getElementById('about-link').classList.add('active'); // Highlight the About link
     }
@@ -30,20 +31,97 @@ function navigateTo(section) {
             <li>2025, "Without Translation," <i>The Margins</i></li>
             <li>2023, <a href="https://www.theseventhwave.org/monique-ouk/2/" target="_blank">"Learning How to Fish Again,"</a> <i>The Seventh Wave</i></li>
             <li>2023, <a href="https://www.theseventhwave.org/monique-ouk/" target="_blank">"Affirmation,"</a> <i>The Seventh Wave</i></li>
-            <!-- Add more writing pieces here -->
           </ul>
         </div>`;
-      document.getElementById('writing-link').classList.add('active'); // Highlight the Writing link
+      document.getElementById('writing-link').classList.add('active'); 
     }
     
     if (section === 'contact') {
-      contentDiv.innerHTML = ''; // Clear content
-      contactFormDiv.style.display = 'block'; // Show the contact form
-      document.getElementById('contact-link').classList.add('active'); // Highlight the Contact link
+      contentDiv.innerHTML = ''; // clear content
+      contactFormDiv.style.display = 'block'; // show the contact form
+      document.getElementById('contact-link').classList.add('active'); 
     }
+
+    if (section === 'khmer') {
+      contentDiv.innerHTML = `
+        <img src="angkorwat.jpg" alt="Angkor Wat | Cambodian-American poets and writers" class="author-image">
+        <div class="bio khmer-essay">
+          <h2>Cambodian-American poets, writers, and others from the diaspora</h2>
+          <ul>
+            <li>Poetry: 
+                <a href="https://www.monicasok.com/" target="_blank">Monica Sok</a>,
+                <a href="https://www.sokunthary.com/about-me" target="_blank">Sokunthary Svay</a>,
+                <a href="https://www.pichchendabao.com/" target="_blank">Pichchenda Bao</a>,
+                <a href="https://www.bunkongtuon.com/" target="_blank">Bunkong Tuon</a>,
+                <a href="https://www.aprillim.com/" target="_blank">Alice Lim</a>
+            </li>
+
+            <li>Fiction:
+                <a href="https://www.harpercollins.com/products/afterparties-anthony-veasna-so?variant=39696421224482" target="_blank">
+                Anthony Veasna So</a>, 
+                <a href="https://vaddeyratner.com/" target="_blank">Vaddey Ratner</a>,
+                <a href="http://www.vichetchum.com/" target="_blank">Vichet Chum</a>,
+                <a href="https://www.instagram.com/smeasuniverse/" target="_blank">Sambath Meas</a>
+            </li>
+
+            <li>Memoir:
+                <a href="https://www.slownoodles.com/" target="_blank">Chanta Nguon</a>,
+                <a href="https://www.putsata.com/" target="_blank">Putsata Reang</a>,
+                <a href="https://www.chanrithyhim.com/" target="_blank">Chanrithy Him</a>,
+                <a href="https://drawnandquarterly.com/author/tian-veasna/" target="_blank">Tian Veasna</a>,
+                <a href="https://www.alicepung.net/" target="_blank">Alice Pung</a>,
+                <a href="https://www.underthenagatail.com/" target="_blank">Mae Bunseng & James Taing</a>,
+                <a href="https://www.maylytao.com/" target="_blank">Mayly Tao</a>,
+                <a href="https://www.loungung.com/" target="_blank">Loung Ung</a>
+            </li>
+           
+
+          
+          <br>
+          <h2>𓆝 𓆟 𓆞</h2>
+          <BR>
+          <p>I first discovered the poet Monica Sok when I was fourteen.</p>
+          <p>At the time, Loung Ung (<i>First They Killed My Father</i>) and Vaddey Ratner 
+          (<i>In the Shadow of the Banyan</i>) were the only Cambodian authors that I knew of. 
+          Almost everything else I came across from the diaspora was centered on genocide. Make no mistake: 
+          this literature is essential. As a pre-teen, I devoured every Cambodian genocide memoir or novel 
+          I could find at my local library, seeking answers to the questions I was too afraid to ask my own parents. 
+          But at some point, the weight of these emotions—shaped by my parents’ survival, our losses, and what 
+          I inherited—grew too heavy to hold. I needed to understand of the rest of my world.</p>
+          <p>When I first googled "cambodian american poet," I was desperate to find someone like me, a 
+          child of genocide survivors, who could help me make sense of the joy and grief and loss and guilt and nostalgia 
+          that I carried.  Monica Sok was the first, and at the time, the only writer I found, but possibly the 
+          most impactful to me since. Here I was, alone in my bedroom, reading words that I swore were taken 
+          from my own life too:
+          </p>
+          <p>
+          "I want to know the names of things, <br>
+          only the names in Khmer. <br>
+          I want to know the names of trees <br>
+          whose roots grow on temple roofs. <br><br>
+
+          I don't want to know my uncle was never found. <br>
+          When my aunts looked for his face, his was not there. <br>
+          […]"
+          <br><br>
+          —"Yearning", <i>Year Zero</i>
+          </p>
+          <p> * </p>
+          <p>
+          I’ve been very fortunate to find other Cambodian-American writers in this past decade 
+          since I first came across Monica’s poetry. I’m in awe of our community and excited for the 
+          stories, novels, poems, and art that we will continue to share with each other and the world. 
+          If you found this page by googling “Cambodian-American poets” or “Cambodian-American writers,” 
+          I hope this list helps you discover more voices, just as I did.
+          </p>
+          <br><br><br>
+        </div>`;
+      document.getElementById('khmer-link').classList.add('active'); 
+    }
+
   }
   
-  // JavaScript for form submission
+  // js for form submission
   document.getElementById('form').addEventListener('submit', function(e) {
     e.preventDefault();
     const form = e.target;
